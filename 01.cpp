b46da136dcd2f3b689d161b092c1dd47c7d32380
Prajjaval Srivastava <prajjavalsrivastava12@gmail.com>
2023-12-27 12:06:49 +0530

    added 01.cpp

---
+++ 01.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    
+    int a = 1;
+    int b = 2;
+    int c = a ^ b;
+    
+    cout<<c<<endl;
+
+    return 0;
+}
